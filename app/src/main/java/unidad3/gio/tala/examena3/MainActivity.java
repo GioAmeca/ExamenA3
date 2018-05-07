a0495efac53a19df1e2eac397ea7e81882241139
gio salazar <36206915+GioAmeca@users.noreply.github.com>
2018-05-07 13:40:13 -0500

    sss

--- app/src/main/java/unidad3/gio/tala/examena3/MainActivity.java
+++ app/src/main/java/unidad3/gio/tala/examena3/MainActivity.java
@@ -1,13 +1,30 @@
 package unidad3.gio.tala.examena3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
-
+   public EditText txtA;
+   public  EditText txtB;
+   public RadioButton suma;
+   public RadioButton resta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtA=(EditText)findViewById(R.id.txtA);
+        txtB=(EditText)findViewById(R.id.txtB);
+    }
+    public void cal(View x){
+        int a=Integer.parseInt(txtA.getText().toString());
+        int b=Integer.parseInt(txtB.getText().toString());
+        int r;
+        r=a+b;
+        Intent m=new Intent(this,op.class);
+      //  m.putExtra("resultado"=r.toString());
     }
 }
