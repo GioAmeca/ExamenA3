94e2cdabdcd640d8e84efbafb8d419094ce8f5e3
gio salazar <36206915+GioAmeca@users.noreply.github.com>
2018-05-07 13:59:20 -0500

    sss

--- app/src/main/java/unidad3/gio/tala/examena3/op.java
+++ app/src/main/java/unidad3/gio/tala/examena3/op.java
@@ -1,13 +1,23 @@
 package unidad3.gio.tala.examena3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class op extends AppCompatActivity {
-
+public TextView operacion;
+public TextView resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_op);
+        operacion=(TextView)findViewById(R.id.txtOperacion);
+        resultado=(TextView)findViewById(R.id.txtResultado);
+        Intent i = getIntent();
+        String opr=i.getStringExtra("resultado");
+        String res=i.getStringExtra("operacion");
+        operacion.setText(opr);
+        resultado.setText("Resultado; "+res);
     }
 }
